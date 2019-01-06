dbe9a29cfe1397352923f802676b8925ddc633cf
devGnode <looterofflux@gmail.com>
2019-01-06 01:42:25 +0100

    Create import.asm

---
+++ import.asm
@@ -0,0 +1,37 @@
+/*
+
+	READ IMPORATION TABLE
+
+	@By Maroder for devGnode
+*/
+[BITS 32]
+
+	global _start
+
+section .data
+section .bss
+
+_start:
+	xor edx, edx		; 
+	mov eax, [fs:edx+0x30]	; TEB
+	mov edi, [eax+0x08]	; ImageBase pattern "MZ"
+	mov eax, [edi+0x3c]	; e_lfnew 0x40
+
+	add eax, edi		; PE
+	mov ebx, [eax+0x80]	; IMPORTATION TABLE
+	add ebx, edi		; IMPORT BASE
+
+	mov esi, dword[ebx]	; RVA 
+	add esi, edi		; Offset + BASE
+	
+	xor ecx, ecx		; calc iteration
+	bcl:	
+	inc ecx
+	inc esi			; IMAGE_IMPORT_BY_NAME [ WORD + FUNCTION NAME ]
+	inc esi			; 
+	lodsd
+	add eax, edi		; RVA + Base	
+	nop			; eax == function name imporation
+	nop
+	nop
+	jmp bcl	
